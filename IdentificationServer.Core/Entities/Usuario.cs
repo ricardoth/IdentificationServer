2241a079072283107e2d3e1a1be4312265ee1c83
ricardoth <ricardotilleriaochoamail.com>
2021-03-15 21:40:40 -0300

    Inicio API en .NET 5 - Clean Architecture

---
+++ IdentificationServer.Core/Entities/Usuario.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IdentificationServer.Core.Entities
+{
+    public class Usuario
+    {
+        public int IdUsuario { get; set; }
+        public string Nombre { get; set; }
+    }
+}
---
+++ IdentificationServer.WebApi/Controllers/UsuarioController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IdentificationServer.WebApi.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsuarioController : ControllerBase
+    {
+        [HttpGet]
+        public IActionResult Get()
+        {
+            List<string> usuarios = new List<string>();
+            usuarios.Add("Ricardo");
+            usuarios.Add("Daniel");
+            return Ok(usuarios);
+        }
+    }
+}
