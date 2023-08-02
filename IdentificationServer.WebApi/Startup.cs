5deb1502dc78670efdacedb8ce125d7725e34507
ricardoth <ricardotilleriaochoa@gmail.com>
2023-08-02 16:10:00 -0400

    Cambios

--- IdentificationServer.WebApi/Startup.cs
+++ IdentificationServer.WebApi/Startup.cs
@@ -45,32 +45,8 @@ namespace IdentificationServer.WebApi
             services.AddSwaggerGen(c =>
             {
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "IdentificationServer.WebApi", Version = "v1" });
-                //c.OperationFilter<AddRequiredHeaderParameter>();
-                //c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
-                //{
-                //    In = ParameterLocation.Header,
-                //    Description = "Por favor, valide el token",
-                //    Name = "Authorization",
-                //    Type = SecuritySchemeType.Http,
-                //    BearerFormat = "JWT",
-                //    Scheme = "Bearer"
-                //});
-                //c.AddSecurityRequirement(new OpenApiSecurityRequirement
-                //{
-                //    {
-                //        new OpenApiSecurityScheme
-                //        {
-                //            Reference = new OpenApiReference
-                //            {
-                //                Type = ReferenceType.SecurityScheme,
-                //                Id = "Bearer"
-                //            }
-                //        },
-                //    new string[] { }
-                //    }
-                //});
             });
-
+            services.AddSwaggerGen();
             services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
 
             services.Configure<PaginationOptions>(Configuration.GetSection("Pagination"));
